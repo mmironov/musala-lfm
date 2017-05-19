81c3e4dbebb3068daf5d49e86060b040634e2ade
Miroslav <miroslavmironov@gmail.com>
2017-05-19 17:46:37 +0300

    Initial Commit

---
+++ ArraysExercise/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ArraysExercise
+//
+//  Created by Miroslav Mironov on 5/19/17.
+//  Copyright © 2017 Miroslav Mironov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
