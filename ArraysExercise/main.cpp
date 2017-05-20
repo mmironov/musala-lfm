c7610421fd57e814f7dee15af80a2e385bfe7dc1
Miroslav <miroslavmironov@gmail.com>
2017-05-20 15:47:17 +0300

    Added a solution for the sum in every row problem.

--- ArraysExercise/main.cpp
+++ ArraysExercise/main.cpp
@@ -6,371 +6,49 @@
 //  Copyright © 2017 Miroslav Mironov. All rights reserved.
 //
 
-#include <iostream>
-#include <cmath>
-using namespace std;
-
-void useTemperatureCalculator();
-
-const double MULT = 9.0;
-const double DIV = 5.0;
-const double ADD = 32.0;
-
-double convertToFahrenheit(double celsius)
-{
-    double result = celsius * MULT / DIV + ADD;
-    return result;
-}
-
-double convertToCelsius(double fahrenheit)
-{
-    double result = (fahrenheit - ADD) * DIV / MULT;
-    return result;
-}
-
-void useTemperatureCalculator()
-{
-    char choice = ' ';
-    double converted = 0;
-    
-    const char choiceF = 'F';
-    const char choiceC = 'C';
-    
-    do
-    {
-        cout << "Enter " << choiceC << " to convert from Celsius" << endl;
-        cout << "Enter " << choiceF << " to convert from Fahreinheit" << endl;
-        cout << "Your choice: ";
-        
-        cin >> choice;
-    } while (!(choice == choiceC || choice == choiceF));
-    
-    double temperature;
-    
-    cout << "Temperature in " << choice << ":";
-    cin >> temperature;
-    
-    if (choice == choiceC)
-    {
-        converted = convertToFahrenheit(temperature);
-    }
-    else if (choice == choiceF)
-    {
-        converted = convertToCelsius(temperature);
-    }
-    
-    const char convertedChoice = (choice == choiceC) ? choiceF : choiceC;
-    
-    cout << temperature << " " << choice << " = " << converted << " " << convertedChoice << endl;
-}
-
-void printPositiveNumbers()
-{
-    int n = 0;
-    
-    do
-    {
-        cout << "n=";
-        cin >> n;
-    } while (n < 0);
-    
-    for(int i=0; i <=n; i += 2)
-    {
-        cout << i << " ";
-    }
-    cout << endl;
-}
-
-void findLongestDuplicate()
-{
-    const int SIZE = 10;
-    int arr[SIZE] = {1, 4, 4, 4, 7, 1, 5, 5, 5, 1};
-    
-    int longest = 1;
-    int lastIndex = 0;
-    
-    int currentLength = 1;
-    
-    for(int i=1; i < SIZE; ++i)
-    {
-        if (currentLength > longest)
-        {
-            longest = currentLength;
-            lastIndex = i-1;
-        }
-        
-        if (arr[i] == arr[i-1])
-        {
-            ++currentLength;
-        }
-        else
-        {
-            currentLength = 1;
-        }
-    }
-    
-    if (currentLength > longest)
-    {
-        longest = currentLength;
-        lastIndex = SIZE - 1;
-    }
-    
-    cout << "Sequence: ";
-    
-    for(int i=lastIndex; i > lastIndex - longest; --i)
-    {
-        cout << arr[i] << " ";
-    }
-    
-    cout << endl;
-}
-
-void findPropertiesForArray()
-{
-    const int SIZE = 1;
-    int arr[SIZE] = {1};
-    
-    bool increasing = true;
-    bool decreasing = true;
-    bool allEqual = true;
-    bool zigZagged = true;
-    
-    bool smallToBig = true;
-    
-    for(int i=1; i < SIZE; ++i)
-    {
-        if (arr[i] < arr[i-1])
-        {
-            increasing = false;
-        }
-        
-        if (arr[i] > arr[i-1])
-        {
-            decreasing = false;
-        }
-
-        if (arr[i] != arr[i-1])
-        {
-            allEqual = false;
-        }
-
-        if ((smallToBig && arr[i] < arr[i-1]) || (!smallToBig && arr[i] > arr[i-1]))
-        {
-            zigZagged = false;
-        }
-        
-        smallToBig = !smallToBig;
-    }
-    
-    cout << "Increasing: " << increasing << endl;
-    cout << "Decreasing: " << decreasing << endl;
-    cout << "All Equal: " << allEqual << endl;
-    cout << "Is zigzagged: " << zigZagged << endl;
-}
-
-bool isPrime(int n)
-{
-    if (n == 1)
-    {
-        return false;
-    }
-    
-    for(int i = 2; i <= n/2; ++i)
-    {
-        if (n % i == 0)
-        {
-            return false;
-        }
-    }
-    
-    return true;
-}
-
-void printPrimesBelow(int n)
-{
-    for(int i=2; i <= n; ++i)
-    {
-        if (isPrime(i))
-        {
-            cout << i << " ";
-        }
-    }
-    cout << endl;
-}
-
-void countLetters(char word[], int n)
-{
-    const int LETTERS_COUNT = 26;
-    int counts[LETTERS_COUNT] = {0};
-    
-//    for(char alpha = 'A'; alpha < 'Z'; ++alpha)
-//    {
-//        for(int i=0; i < n; ++i)
-//        {
-//            if (word[i] == alpha)
-//            {
-//                ++counts[alpha - 'A'];
-//            }
-//        }
-//    }
-    
-    
-    for(int i=0; i < n; ++i)
-    {
-        char alpha = word[i];
-        
-        ++counts[alpha - 'A'];
-    }
-    
-    for(int i=0; i < LETTERS_COUNT; ++i)
-    {
-        if (counts[i] != 0)
-        {
-            cout << (char)(i + 'A') << ": " << counts[i] << endl;
-        }
-    }
-}
-
-bool cointainsDuplicatesInSorted(int arr[], int n)
-{
-    for(int i=1; i < n; ++i)
-    {
-        if (arr[i-1] == arr[i])
-        {
-            return true;
-        }
-    }
-    
-    return false;
-}
-
-bool cointainsDuplicates(int arr[], int n)
-{
-    for(int i=0; i < n-1; ++i)
-    {
-        for(int j = i+1; j < n; ++j)
-        {
-            if (arr[i] == arr[j])
-            {
-                return true;
-            }
-        }
-    }
-    
-    return false;
-}
-
-void printDuplicates(int arr[], int n)
-{
-    for(int i=0; i < n-1; ++i)
-    {
-        for(int j = i+1; j < n; ++j)
-        {
-            if (arr[i] == arr[j])
-            {
-                bool alreadyCounted = false;
-                for(int k=i-1; k >= 0; --k)
-                {
-                    if (arr[k] == arr[i])
-                    {
-                        alreadyCounted = true;
-                        break;
-                    }
-                }
-                
-                if (!alreadyCounted) {
-                    cout << arr[i] << " ";
-                    break;
-                }
-            }
-        }
-    }
-}
-
-void printDuplicatesWithCounting(int arr[], int n)
-{
-    int count = 0;
-    
-    for(int i=0; i < n-1; ++i)
-    {
-        count = 1;
-        for(int j = i+1; j < n; ++j)
-        {
-            if (arr[i] == arr[j])
-            {
-                ++count;
-            }
-        }
-        
-        if (count == 2)
-        {
-            cout << arr[i] << " ";
-        }
-    }
-}
-
-bool eq(double d1, double d2, double eps = 0.000000001)
-{
-    return abs(d1 - d2) < eps;
-}
-
-double sqrt(double x, double eps)
-{
-    double low = 0;
-    double high = x;
-    
-    double mid = x/2;
-    
-    while (!eq(mid*mid, x, eps))
-    {
-        if (mid*mid > x)
-        {
-            high = mid;
-        }
-        
-        if (mid*mid < x)
-        {
-            low = mid;
-        }
-        
-        mid = (high + low) / 2;
-    }
-    
-    return mid;
-}
+#include "arrays.h"
+#include "matrices.h"
 
 int main(int argc, const char * argv[]) {
-    
+//    
 //    useTemperatureCalculator();
 //    printPositiveNumbers();
 //    findLongestDuplicate();
 //    findPropertiesForArray();
-    
+//    
 //    cout << isPrime(131) << endl;
-
+//
 //    printPrimesBelow(1000000);
-    
+//    
 //    const int SIZE = 5;
 //    char word[SIZE] = {'H', 'E', 'L', 'L', 'O'};
 //    
 //    countLetters(word, SIZE);
 //    
-    double d1 = 4.99999;
-    double d2 = 4.99998;
+//    double d1 = 4.99999;
+//    double d2 = 4.99998;
+//    
+//    if (eq(d1, d2, 0.01))
+//    {
+//        cout << "equal" << endl;
+//    }
+//    
+//    const int N = 7;
+//    int numbers[N] = {1, 2, 3, 4, 5, 6, 7};
+//    
+//    printDuplicatesWithCounting(numbers, N);
+//    cout << endl;
+//    
+//    double d = 20;
+//    cout << "sqrt(" << d << ") = " << sqrt(d, 0.0001) << endl;
+
+    const int SSIZE = 2;
+    int matrix[SSIZE][3] = {{1, 2, 3}, {4, 5, 6}};
     
-    if (eq(d1, d2, 0.01))
-    {
-        cout << "equal" << endl;
-    }
+    printMatrixByColumns(matrix, SSIZE);
+    cout << sumAll(matrix, SSIZE) << endl;
     
-    const int N = 7;
-    int numbers[N] = {1, 2, 3, 4, 5, 6, 7};
-    
-    printDuplicatesWithCounting(numbers, N);
-    cout << endl;
-    
-    double d = 20;
-    cout << "sqrt(" << d << ") = " << sqrt(d, 0.0001) << endl;
+    printSumInEveryRow(matrix, SSIZE);
     
     return 0;
 }
--- ArraysExercise/matrices.h
+++ ArraysExercise/matrices.h
@@ -9,5 +9,64 @@
 #ifndef matrices_h
 #define matrices_h
 
+#include <iostream>
+using namespace std;
+
+void printRow(int row[], int n)
+{
+    for(int i=0; i < n; ++i)
+    {
+        cout << row[i] << " ";
+    }
+}
+
+void printMatrix(int matrix[][3], int n)
+{
+    for(int row = 0; row < n; ++row)
+    {
+        printRow(matrix[row], 3);
+        cout << endl;
+    }
+}
+
+void printMatrixByColumns(int matrix[][3], int n)
+{
+    for(int col = 0; col < 3; ++col)
+    {
+        for(int row = 0; row < n; ++row)
+        {
+            cout << matrix[row][col] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int sumAll(int matrix[][3], int n)
+{
+    int sum = 0;
+    
+    for(int col = 0; col < 3; ++col)
+    {
+        for(int row = 0; row < n; ++row)
+        {
+            sum += matrix[row][col];
+        }
+    }
+    
+    return sum;
+}
+
+void printSumInEveryRow(int matrix[][3], int n)
+{
+    for(int row=0; row < n; ++row)
+    {
+        int sum = 0;
+        for(int col=0; col < 3; ++col)
+        {
+            sum += matrix[row][col];
+        }
+        cout << "Row " << row << ": " << sum << endl;
+    }
+}
 
 #endif /* matrices_h */
