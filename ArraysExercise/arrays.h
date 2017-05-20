14df7f2a8a36a59d68bc317fcba7bf7a04c8ad1a
Miroslav <miroslavmironov@gmail.com>
2017-05-20 15:32:05 +0300

    Added the first solution for matrices.

---
+++ ArraysExercise/arrays.h
@@ -0,0 +1,13 @@
+//
+//  Header.h
+//  ArraysExercise
+//
+//  Created by Miroslav Mironov on 5/20/17.
+//  Copyright © 2017 Miroslav Mironov. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+
+#endif /* Header_h */
---
+++ ArraysExercise/matrices.h
@@ -0,0 +1,13 @@
+//
+//  matrices.h
+//  ArraysExercise
+//
+//  Created by Miroslav Mironov on 5/20/17.
+//  Copyright © 2017 Miroslav Mironov. All rights reserved.
+//
+
+#ifndef matrices_h
+#define matrices_h
+
+
+#endif /* matrices_h */
